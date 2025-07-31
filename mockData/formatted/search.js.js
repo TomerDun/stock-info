872424ab45c86b7a5f4cacc46adeb309fba51e7e
TomerDun <tomer@dunsky.name>
2025-07-31 10:49:43 +0300

    Added some mock data

---
+++ mockData/formatted/search.js.js
@@ -0,0 +1,26 @@
+export const search_AA = [
+  {
+    symbol: 'AAPU',
+    name: 'Direxion Daily AAPL Bull 1.5X Shares',
+    exchange: 'NASDAQ'
+  },
+  { symbol: 'AAPL', name: 'Apple Inc.', exchange: 'NASDAQ' },
+  {
+    symbol: 'AAPG',
+  },
+  {
+    symbol: 'AAPD',
+    name: 'Direxion Daily AAPL Bear 1X Shares',
+    exchange: 'NASDAQ'
+  },
+  {
+    symbol: 'AAPB',
+    name: 'GraniteShares ETF Trust - GraniteShares 2x Long Tilray Daily ETF',
+    exchange: 'NASDAQ'
+  },
+  {
+    symbol: 'CAAPX',
+    name: 'Ariel Appreciation Fund',
+    exchange: 'NASDAQ'
+  }
+]
